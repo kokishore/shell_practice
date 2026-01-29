a9269a5aae508e8da85b17c5ca8a0f086af2da00
kishore Kothapalli <kishore.kothapalli97@gmail.com>
2026-01-29 11:59:55 +0530

    hellow

--- 04-variables.sh
+++ 04-variables.sh
@@ -4,6 +4,6 @@ P1=$1 #there should be no space before and after =
 P2=$2
 
 echo "$P1 : Hi $P2, How are you ? "
-echo "$P2 : Hi $P2 garu , Iam fine  How are you  sir ? "
+echo "$P2 : Hi $P1 garu , Iam fine  How are you  sir ? "
 echo "$P1 : Yeah Iam fine ! We will work together to develop the state "
 echo "$P2 : Sure Definetly Sir "
