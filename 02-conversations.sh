6909cc0058958eb944b10a632fb5409d4ac4884d
kishore Kothapalli <kishore.kothapalli97@gmail.com>
2026-01-29 11:48:43 +0530

    hellow

---
+++ 02-conversations.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "CBN : Hi PSPK , How are you ? "
+echo "PSPK : Hi CBN garu , Iam fine  How are you  sir ? "
+echo "CBN : Yeah Iam fine ! We will work together to develop the state "
+ech  "PSPK : Sure Definetly Sir 
