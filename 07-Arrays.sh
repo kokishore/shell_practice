2220ba784c9983a6365fe0031b6e26b64b9eee91
kishore Kothapalli <kishore.kothapalli97@gmail.com>
2026-01-31 11:19:07 +0530

    hellow

---
+++ 07-Arrays.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+Movies=("Rajasaab" "NNM" "BMW" "AAK" "MSVP")
+
+echo " First thing :${Movies[0]}"
+
+echo " Second thing : ${Movies[1]}"
+
+echo " Third thing : ${Movies[2]}"
+
+echo " All things : ${Movies[@]}"
