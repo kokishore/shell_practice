1d021422f76d98221a69258b2861dfa0173d4076
kishore Kothapalli <kishore.kothapalli97@gmail.com>
2026-01-31 12:28:06 +0530

    hellow

---
+++ 10-commands_Instalation.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ]
+then
+ echo " error : Please run this script with root access"
+ exit 1
+else
+ echo " You are running this script with root accees"
+fi
+
+dnf list installed mysql
+
+if [ $? -ne 0 ]
+then 
+ echo " MYSQL is not installed .... Going to install it."
+      dnf install mysql -y
+
+      if [ $? -eq 0 ]
+      then
+       echo "MySQl installation is SUCCESS"
+      else
+       echo "Mysql installation is Failure"
+else
+ echo " MYSQL is  installed .... Nothing to do"
